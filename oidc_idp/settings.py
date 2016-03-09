c56167aa6e954a044e0ace75d3f47401b806858b
Antony <antony@localhost.localdomain>
2016-03-10 00:50:43 +0530

    removing host dependency.

--- oidc_idp/settings.py
+++ oidc_idp/settings.py
@@ -119,3 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+import logging
+
+logging.basicConfig(
+    level = logging.DEBUG,
+    format = " %(levelname)s %(name)s: %(message)s",
+)
+
